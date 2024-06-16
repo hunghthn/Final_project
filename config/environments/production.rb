8a0fe2d389e176c39485b6769df2604237a33476
hunghthn <hunght@zigexn.vn>
2024-06-16 22:13:00 +0700

    Precompiled assets 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.assets.version = '1.0'
   config.serve_static_assets = true
