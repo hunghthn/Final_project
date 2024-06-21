2caf94d3bc915b2fbf49f3917e29f3dc65de5ee1
hunghthn <hunght@zigexn.vn>
2024-06-21 21:27:27 +0700

    fix bug

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,22 +1,3 @@
-# class InquiriesController < ApplicationController
-#     def create
-#       @inquiry = Inquiry.new(inquiry_params)
-#       if @inquiry.save
-#         flash[:notice] = "Yêu cầu được tạo thành công."
-#         redirect_to request.referer
-#       else
-#         flash[:alert] = "Đã xảy ra lỗi khi tạo yêu cầu."
-#         redirect_to request.referer
-#       end
-#     end
-  
-#     private
-  
-#     def inquiry_params
-#       params.require(:inquiry).permit(:name, :email, :phone, :note, :preferred_contact, :car_dealer_car_id ,:user_id)
-#     end
-#   end
-  
 class InquiriesController < ApplicationController
   def create
     @inquiry = Inquiry.new(inquiry_params)
